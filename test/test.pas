ffa3d35931f9a578b41c93aa8fb830ad1119abae
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2023-01-30 18:05:19 +0800

    fix (test)

--- test/test.pas
+++ test/test.pas
@@ -1,8 +1,8 @@
 program test;
 var f:file of String;s:String;
 begin
-    Assign('asset/easy.txt');
-    reset(f);
+    assign('asset/txt/easy.txt');
+    reset(f)    ;
     while not(EOF()) do
     begin
         while not(EOLn()) do
