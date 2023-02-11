21441ed1dfeac968093b44f4e237bd79375cab69
Jason Chan <80382634+jasonchanjj123@users.noreply.github.com>
2023-02-11 10:18:06 +0000

    Revert "fix"
    This reverts commit 7748d5fcc52d42ef3776d76c0e43155ff66a39ae.

--- game/filing.pas
+++ game/filing.pas
@@ -1,30 +1,29 @@
 unit filing;
 
 interface
-    function Get_table(level:integer);
-
-implementation
 var Table:file of String ;level_type,table:string;
-    function Get_table(level:integer):String;
-        var l,lines,max_lines,target:integer;temp:string;
+    function get_table(level:integer):String;
+    var l,lines:integer;temp:string;
+    begin
+        randomize;
+        table:='';
+        case (level) of 
+            1:level_type:='easy.txt';
+            2:level_type:='hard.txt';
+            3:level_type:='hardest.txt';
+        end;
+
+        Assign(Table,'asset/txt/'+level_type);
+        while not EOLn(Table) do 
+            Read(lines);
+        target :=random(lines)+1;
+        for l:=1 to target do
         begin
-            randomize;
-            table:='';
-            case (level) of 
-                1:level_type:='easy.txt';
-                2:level_type:='hard.txt';
-                3:level_type:='hardest.txt';
-            end;
+            Readln(temp);
+            if l<>target then 
+                continue;
+            table:=table+temp;
+        end; 
+        get_table:=table;
 
-            Assign(table,'asset/txt/'+level_type);
-            Reset(f);
-            readln(f,max_lines);
-
-            target:=random(max_lines-1)+1; 
-            for l:= 1 to target do
-                ReadLn(f);
-            readln(f,table);
-            Close(f);
-            get_table:=table;
-
-    end;
+end;
